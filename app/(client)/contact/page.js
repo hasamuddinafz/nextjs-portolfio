eaa1d4e063c19790e3ff8be091d5979ec24a58c2
Hasamuddin Afzali <hasamuddin.afz@gmail.com>
2024-07-13 14:13:42 +0300

    first commit

--- app/(client)/contact/page.js
+++ app/(client)/contact/page.js
@@ -20,7 +20,7 @@ function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:3000/api/contacts/`, {
+      const response = await fetch(`/api/contacts/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
