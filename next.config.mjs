180dd4d7337cccc0ea6b625f647d232640ffaa35
Hasamuddin Afzali <hasamuddin.afz@gmail.com>
2024-07-15 12:07:08 +0300

    used DOMPURIFY for show the html content as a plain text

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['miro.medium.com'],
     },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      },
 };
 
 export default nextConfig;
